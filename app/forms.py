ee2749d5f583fc69254ebc0cd0130e8ffc0a6277
tomshaffery <98631104+tomshaffery@users.noreply.github.com>
2025-04-10 02:40:28 -0400

    Search form, search page

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, BooleanField
+from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
 class SearchForm(FlaskForm):
-    
+    friend = StringField('Friend', validators=[DataRequired()])
+    submit = SubmitField('Find Games')
