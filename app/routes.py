33622ab22f2ac27034e116ede0c4601264af8e21
tomshaffery <98631104+tomshaffery@users.noreply.github.com>
2025-04-10 02:53:30 -0400

    Search form, route updated

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,13 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import SearchForm
 
 @app.route('/')
 @app.route('/index')
 def index():
    return render_template('index.html', title='Play With Friends')
+
+@app.route('/search')
+def search():
+   form = SearchForm()
+   return render_template('search.html', title='Find New Games', form=form)
