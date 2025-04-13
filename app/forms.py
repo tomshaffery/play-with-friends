733799c992f5781d697b46f0a4df355f784bfeca
tomshaffery <98631104+tomshaffery@users.noreply.github.com>
2025-04-12 23:40:29 -0400

    Added User to form

--- app/forms.py
+++ app/forms.py
@@ -3,5 +3,6 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
 class SearchForm(FlaskForm):
+    user = StringField('You', validators=[DataRequired()])
     friend = StringField('Friend', validators=[DataRequired()])
     submit = SubmitField('Find Games')
